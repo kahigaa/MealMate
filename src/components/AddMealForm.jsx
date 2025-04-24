31fb607b80a865e2ae0ef3a100a6c225dca1c528
ElvisAmonde <ogollanyanja@gmail.com>
2025-04-24 13:54:38 +0300

    functions for inputs

---
+++ src/components/AddMealForm.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import  {useNavigate} from 'react-router-dom';
+import axos from 'axios'
+
+const AddMeal - () => {
+    const navigate = useNavigate();
+     //form inputs and errros
+     const [ formData, setFormData] = useState({
+        day:''
+        name:''
+        ingredients:''
+        imageUrl:''
+        recipeLink:''
+
+     });
+     const [error. setError] = useSate('');
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
