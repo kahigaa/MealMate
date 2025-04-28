9d83e8ff8657ecf1d945ec1c27de56d496fa529e
ElvisAmonde <ogollanyanja@gmail.com>
2025-04-28 10:09:02 +0300

    added onedit props and added to the map function

--- src/components/MealHolder.jsx
+++ src/components/MealHolder.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import MealCard from './MealCard';
-import '../MealHolder.css';
+import React from "react";
+import MealCard from "./MealCard";
+import "../MealHolder.css";
 
-const MealHolder = ({ meals }) => {
+const MealHolder = ({ meals, onEdit }) => {
   return (
     <div className="meal-holder-container">
       <h2 className="meal-holder-title">Your Meals</h2>
       <div className="meal-cards-wrapper">
         {meals.length > 0 ? (
           meals.map((meal, index) => (
-            <MealCard key={`${meal.id}-${index}`} meal={meal} />
+            <MealCard key={`${meal.id}-${index}`} meal={meal} onEdit={onEdit} />
           ))
         ) : (
           <p className="no-meals-message">No meals added yet</p>
