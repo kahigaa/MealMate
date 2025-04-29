4fc449f82d889dfc420b2879eb31105bcb755650
Michael Amonde <michaelamonde@Michaels-MacBook-Pro.local>
2025-04-29 13:37:23 +0300

    added  delete functionality

--- src/components/MealHolder.jsx
+++ src/components/MealHolder.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import MealCard from "./MealsCard";
 import "../MealHolder.css";
 
-const MealHolder = ({ meals, onEdit }) => {
+const MealHolder = ({ meals, onEdit, onDelete }) => {
   return (
     <div className="meal-holder-container">
       <h2 className="meal-holder-title">Your Meals</h2>
       <div className="meal-cards-wrapper">
         {meals.length > 0 ? (
           meals.map((meal, index) => (
-            <MealCard key={`${meal.id}-${index}`} meal={meal} onEdit={onEdit} />
+            <MealCard
+              key={`${meal.id}-${index}`}
+              meal={meal}
+              onEdit={onEdit}
+              onDelete={onDelete}
+            />
           ))
         ) : (
           <p className="no-meals-message">No meals added yet</p>
